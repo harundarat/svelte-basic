1e9f263369e69c846048ec72e82e24c3d92ca565
harundarat <harundarat@gmail.com>
2025-10-13 07:53:50 +0700

    derived

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,9 +1,18 @@
 <script>
-  let count = $state(0);
+  let count = $state([]);
+  // let total = $derived(count.reduce((prev, curr) => prev + curr, 0));
 
-  const increment = () => count++;
+  let total = $derived.by(() => {
+    let total = 0;
+    for (let i = 0; i < count.length; i++) {
+      total += count[i];
+    }
+    return total;
+  });
+
+  const increment = () => count.push(1);
 </script>
 
-<h1>Count: {count}</h1>
+<h1>{count.join(" + ")} = {total}</h1>
 
 <button onclick={increment}>Increment</button>
