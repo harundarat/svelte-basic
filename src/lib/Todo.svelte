de20cb7072ccbc4c94ba03779768206756c3bcc3
harundarat <harundarat@gmail.com>
2025-10-16 12:04:52 +0700

    event

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -1,19 +1,5 @@
 <script>
-  const { id, name, done } = $props();
-
-  const emojis = {
-    "1": "💻",
-    "2": "🎨",
-    "3": "📖",
-    "4": "🚀",
-  };
-
-  const emoji = emojis[id];
+  const { id, name } = $props();
 </script>
 
-{emoji}
-{#if done}
-  <strike>{id} - {name}</strike>
-{:else}
-  {id} - {name}
-{/if}
+{id} - {name}
--- src/lib/TodoApp.svelte
+++ src/lib/TodoApp.svelte
@@ -1,40 +1,58 @@
 <script>
+  import EditTodo from "./EditTodo.svelte";
   import Todo from "./Todo.svelte";
 
-  const data = $state([
-    {
-      id: "1",
-      name: "Belajar HTML",
-      done: true,
-    },
-    {
-      id: "2",
-      name: "Belajar CSS",
-      done: false,
-    },
-    {
-      id: "3",
-      name: "Belajar Javascript",
-      done: true,
-    },
-    {
-      id: "4",
-      name: "Belajar Svelte",
-      done: false,
-    },
-  ]);
+  let data = $state([]);
 
-  const remove = () => {
-    data.shift();
+  let id = 0;
+
+  const add = (e) => {
+    e.preventDefault();
+
+    const input = document.getElementById("todo");
+    data.push({
+      id: id++,
+      name: input.value,
+    });
+    input.value = "";
+  };
+
+  const remove = (id) => {
+    data = data.filter((todo) => todo.id !== id);
+  };
+
+  const edit = (id) => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        data[i] = { ...data[i], edit: true };
+      }
+    }
+  };
+
+  const onEdit = (id, name) => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        data[i] = { id, name, edit: false };
+      }
+    }
   };
 </script>
 
-<button onclick={remove}>Remove</button>
+<form>
+  <input type="text" id="todo" />
+  <button onclick={add}>Add</button>
+</form>
 
 <ul>
   {#each data as todo (todo.id)}
     <li>
-      <Todo {...todo} />
+      {#if todo.edit}
+        <EditTodo id={todo.id} name={todo.name} onedit={onEdit} />
+      {:else}
+        <Todo {...todo} />
+        <button onclick={() => edit(todo.id)}>Edit</button>
+        <button onclick={() => remove(todo.id)}>Remove</button>
+      {/if}
     </li>
   {/each}
 </ul>
