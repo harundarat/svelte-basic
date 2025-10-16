9a7f78610f6fe7a886c8879847ff334d9dc7d68b
harundarat <harundarat@gmail.com>
2025-10-16 07:03:47 +0700

    props

---
+++ src/lib/UserApp.svelte
@@ -0,0 +1,22 @@
+<script>
+  import UserRow from "./UserRow.svelte";
+
+  const user = {
+    id: 1,
+    name: "Harun",
+    address: "Indonesia",
+  };
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Address</th>
+    </tr>
+  </thead>
+  <tbody>
+    <UserRow {...user} />
+  </tbody>
+</table>
