d629c134c57c09adc7f38aaca35e98aae23bd3f3
harundarat <harundarat@gmail.com>
2025-10-16 08:39:05 +0700

    await block

---
+++ src/lib/Article.svelte
@@ -0,0 +1,15 @@
+<script>
+  const getArticle = async () => {
+    let response = await fetch("/article.json");
+    return await response.json();
+  };
+</script>
+
+{#await getArticle()}
+  <p>Loading...</p>
+{:then article}
+  <p>Title: {article.title}</p>
+  <p>Content: {article.content}</p>
+{:catch err}
+  <p>Error: {err.message}</p>
+{/await}
