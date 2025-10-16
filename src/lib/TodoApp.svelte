0bc6ad32ad9380936d62e4aebfeeb6f4e8d53b9d
harundarat <harundarat@gmail.com>
2025-10-16 07:16:17 +0700

    if block

---
+++ src/lib/TodoApp.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Todo from "./Todo.svelte";
+</script>
+
+<ul>
+  <li>
+    <Todo id="1" name="Belajar HTML" done={true} />
+  </li>
+  <li>
+    <Todo id="2" name="Belajar CSS" done={false} />
+  </li>
+  <li>
+    <Todo id="3" name="Belajar Javascript" done={false} />
+  </li>
+</ul>
